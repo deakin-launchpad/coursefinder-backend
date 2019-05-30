842e20f9b51df11e7b454faac527d7c1eaaba1bf
Vibhav-Deo <vdeo@deakin.edu.au>
2019-05-30 17:03:54 +1000

    index file importing services

---
+++ DB/index.js
@@ -0,0 +1,13 @@
+const MODEL = require('../Model')
+const DBSERVICE = require('./databaseService')
+
+const SERVICES = {
+
+    AGENTSERVICE: new DBSERVICE('AGENT'),
+    STUDENTSERVICE: new DBSERVICE('STUDENT'),
+    COURSESERVICE: new DBSERVICE('COURSE'),
+    NEWSSERVICE: new DBSERVICE('NEWS')
+
+}
+
+module.exports = SERVICES
