90a21dbc7d8ba124eb70b6af1fe9172ef6444cbb
Vibhav-Deo <vdeo@deakin.edu.au>
2019-05-30 17:05:10 +1000

    helper file for db

---
+++ Utils/dbHelper.js
@@ -0,0 +1,62 @@
+const debug = require('debug')('app:dbHelper')
+require('dotenv').config();
+var DB_OBJECT;
+
+function getConnector(db) {
+  var connector = null;
+  switch (db.adapter) {
+    case 'mongodb':
+      connector = process.env.MONGO_URI || 
+            `mongodb://${db.username}:${db.password}@${db.host}:${db.port}/${db.database}`;
+  }
+  return connector;
+}
+
+function connectMongoose(db, callback) {
+  console.log('DATABASE')
+  var mongoose = require('mongoose');
+  mongoose.connect(getConnector(db),
+  function (err) {
+    if (err) {
+        debug("DB Error: ", err);
+        process.exit(1);
+    } else {
+        debug('MongoDB connected, database:', db.database);
+        DB_OBJECT = mongoose;
+        callback();
+    }
+  });
+}
+
+function dropMongooseDatabase(callback) {
+  DB_OBJECT.connection.db.dropDatabase((err) => {
+    if (err) callback(err)
+    else callback()
+  });
+}
+
+function establishConnection(db, callback) {
+    console.log('[CONNECTION]',db) 
+  switch (db.adapter) {
+    case 'mongodb':
+      connectMongoose(db, callback);
+      break;
+  }
+}
+
+function destroyConnection(db) {
+  
+}
+
+function dropDatabase(db, callback) {
+  switch (db.adapter) {
+    case 'mongodb':
+      dropMongooseDatabase(callback);
+      break;
+  }
+}
+
+module.exports = {
+  establishConnection: establishConnection,
+  dropDatabase: dropDatabase
+};
