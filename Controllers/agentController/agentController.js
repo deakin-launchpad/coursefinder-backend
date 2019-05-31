b22c9e579dde05e64913dfee316db01a57cbda53
Vibhav-Deo <vdeo@deakin.edu.au>
2019-05-31 17:23:36 +1000

    modified existing controller with the new populate function

--- Controllers/agentController/agentController.js
+++ Controllers/agentController/agentController.js
@@ -13,7 +13,7 @@ var registerAgent = function (payloadData, callback) {
 
   async.series([
 
-    function (cb) {                                                                         
+    function (cb) {
       userData = payloadData
       const criteria = { email: userData.email }
       SERVICES.AGENTSERVICE.getRecord(criteria, {}, {}, (err, data) => {
@@ -58,73 +58,91 @@ var agentLogin = function (payloadData, callback) {
       return callback(ERROR.NOT_FOUND)
     }
   })
-  
+
 };
 
 const getAllAgents = function (callback) {
-    const criteria = {}
-    const projections = {
-      '_id': 0,
-      'password': 0
+  const criteria = {}
+  const projections = {
+    'password': 0,
+    '__v': 0
+  }
+  var path = "interestedStudents"
+  var select = ""
+  var populate = {
+    path: path,
+    match: {},
+    select: select,
+    options: { lean: true }
+  }
+
+  SERVICES.AGENTSERVICE.populatedRecords(criteria, projections, populate, {}, {}, (err, data) => {
+    if (err) return callback(err)
+
+    else if (data && data.length > 0) return callback(null, data)
+
+    else return callback(ERROR.USER_NOT_FOUND)
+  })
+}
+
+const getAgent = function (payloadData, callback) {
+  var userData = payloadData
+  const criteria = { _id: payloadData }
+  const projections = {
+    'password': 0,
+    '__v': 0
+  }
+
+  var path = "interestedStudents"
+  var select = ""
+  var populate = {
+    path: path,
+    match: {},
+    select: select,
+    options: { lean: true }
+  }
+
+  SERVICES.AGENTSERVICE.populatedRecords(criteria, projections, populate, {}, {}, (err, data) => {
+    if (err) return callback(err)
+
+    else if (data && data.length > 0) return callback(null, data)
+
+    else return callback(ERROR.USER_NOT_FOUND)
+  })
+}
+
+
+const updateAgent = function (payloadData, callback) {
+  var userData;
+  async.series([
+
+    function (cb) {
+      userData = payloadData
+      const criteria = { email: userData.email }
+      SERVICES.AGENTSERVICE.getRecord(criteria, {}, {}, (err, data) => {
+        if (err) cb(err)
+        else if (data && data.length > 0) cb()
+        else cb(ERROR.USER_NOT_FOUND)
+      })
+    },
+    function (cb) {
+      userData = payloadData
+      const criteria = { _id: payloadData.id }
+
+      SERVICES.AGENTSERVICE.updateRecord(criteria, userData, function (err, data) {
+        if (err) cb(err)
+        else {
+          userData = data
+          cb()
+        }
+      })
     }
-  
-    SERVICES.AGENTSERVICE.getRecord(criteria, projections, {}, (err, data) => {
-      if (err) return callback(err)
-  
-      else if (data && data.length > 0) return callback(null, data)
-  
-      else return callback(ERROR.USER_NOT_FOUND)
-    })
-  }
-  
-  const getAgent = function (payloadData, callback) {
-    var userData = payloadData
-    const criteria = { _id: payloadData }
-    const projections = {
-      '_id': 0,
-      'password': 0
-    }
-    SERVICES.AGENTSERVICE.getRecord(criteria, projections, {}, (err, data) => {
-      if (err) return callback(err)
-  
-      else if (data && data.length > 0) return callback(null, data)
-  
-      else return callback(ERROR.USER_NOT_FOUND)
-    })
-  }
-  
-  
-  const updateAgent = function(payloadData, callback){
-    var userData;
-    async.series([
-  
-      function (cb) {
-        userData = payloadData
-        const criteria = { email: userData.email }
-        SERVICES.AGENTSERVICE.getRecord(criteria, {}, {}, (err, data) => {
-          if (err) cb(err)
-          else if (data && data.length > 0) cb()
-          else cb(ERROR.USER_NOT_FOUND)
-        })
-      },
-      function (cb) {
-        userData = payloadData
-        const criteria = {_id: payloadData.id}
-        
-        SERVICES.AGENTSERVICE.updateRecord(criteria, userData, function (err, data) {
-          if (err) cb(err)
-          else {
-            userData = data
-            cb()
-          }
-        })
-      }
-  
-    ], function (err, result) {
-      if (err) return callback(err)
-      else return callback(null, userData)
-    })
-  }
+
+  ], function (err, result) {
+    if (err) return callback(err)
+    else return callback(null, userData)
+  })
+}
 
 module.exports = {
   registerAgent: registerAgent,
