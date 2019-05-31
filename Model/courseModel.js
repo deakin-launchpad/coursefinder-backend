2233e450862f174d25abc46e40967da9f434607a
Vibhav-Deo <vdeo@deakin.edu.au>
2019-05-31 17:26:23 +1000

    removed id to suite mongo schema practices

--- Model/courseModel.js
+++ Model/courseModel.js
@@ -5,14 +5,12 @@ var Schema = mongoose.Schema;
 
 
 var courseSchema = new Schema({
-  id: { type: Number, unique: true, min: 1 },
   name: {type: String, required: true},
   university:{ name: {type: String, required: true}, logo: {type:String}},
   contact:{ type: String},
   fees: {type: Number, required: true},
   department: {type: String, required: true},
-  website: { type: String}, 
- 
+  website: { type: String} 
 });
 
 
