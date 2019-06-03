d2129213fa3ad393e28c103cd62dd5aba416ac85
Vibhav-deo <vdeo@deakin.edu.au>
2019-06-03 17:51:40 +1000

    new route handler functions

--- Controllers/courseController/courseController.js
+++ Controllers/courseController/courseController.js
@@ -22,7 +22,73 @@ const insertCourse = function (payloadData, callback) {
   
   };
 
+  const getAllCourses = function (callback) {
+    var courseData;
+    const criteria = {}
+    const projection = {}
+    SERVICES.COURSESERVICE.getRecord(criteria, projection, {}, function (err, data) {
+        if (err) return callback(err)
+        else if(data && data.length > 0){
+          courseData = data
+            return callback(null, courseData)
+      }
+      else return callback(null, ERROR.DEFAULT)
+    }) 
+  
+  };
+
+  const getCourse = function (payloadData, callback) {
+    var courseData;
+    const criteria = {_id: payloadData}
+    const projection = {
+      '__v':0
+    }
+    SERVICES.COURSESERVICE.getRecord(criteria, projection, {}, function (err, data) {
+        if (err) return callback(err)
+        else if(data && data.length > 0){
+          courseData = data
+            return callback(null, courseData)
+      }
+      else return callback(null, ERROR.DEFAULT)
+    }) 
+  
+  };
+
+
+  const updateCourse = function (payloadData, callback) {
+    var courseData;
+    const criteria = {_id: payloadData.id}
+    courseData = payloadData
+    async.series([
+      function(cb){
+        SERVICES.COURSESERVICE.getRecord(criteria, projection, {}, function (err, data) {
+          if (err) return cb(err)
+          else if(data && data.length > 0){
+            courseData = data
+              return cb(null, courseData)
+        }
+        else return cb(null, ERROR.DEFAULT)
+      }) 
+      },
+      function(cb){
+        SERVICES.COURSESERVICE.updateRecord(criteria, courseData,function (err, data) {
+          if (err) return cb(err)
+          else {
+              courseData = data
+              return cb(null, courseData)
+        }
+      }) 
+      }
+    ], function (err, result) {
+      if (err) return callback(err)
+      else return callback(null, userData)
+    })  
+  };
+
   module.exports = {
-    insertCourse: insertCourse
+    insertCourse: insertCourse,
+    getAllCourses: getAllCourses,
+    updateCourse: updateCourse,
+    getCourse: getCourse
   };
   
