7596aa12c043bee9649dfbd5a08b725ca27be494
Vibhav-deo <vdeo@deakin.edu.au>
2019-06-03 17:52:37 +1000

    modified model with new keys

--- Model/courseModel.js
+++ Model/courseModel.js
@@ -6,12 +6,12 @@ var Schema = mongoose.Schema;
 
 var courseSchema = new Schema({
   name: {type: String, required: true},
-  university:{ name: {type: String, required: true}, logo: {type:String}},
+  description: {type: String, required: true},
+  university:{ name: {type: String, required: true}, logo: {type:String}, location: {type: String, required: true}},
   contact:{ type: String},
+  courseLevel: {type: String, required: true},
   fees: {type: Number, required: true},
   department: {type: String, required: true},
   website: { type: String} 
 });
-
-
 module.exports = mongoose.model('course', courseSchema);
