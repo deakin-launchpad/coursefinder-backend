a23ef63c09c5f47ba626bcbbe8366b53f045433e
Vibhav-deo <vdeo@deakin.edu.au>
2019-06-03 17:53:24 +1000

    new key value in key content

--- Model/newsModel.js
+++ Model/newsModel.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 var newsSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String },
-    content: { type: { type: String, required: true, enum: ['HTML', 'VIDEO', 'LINKS'] } },
+    content: { type: { type: String, required: true, enum: ['HTML', 'VIDEO', 'LINKS'] }, value:{ type: String, required: true} },
     tags: { type: String, required: true }, //University Name, department so we can filter it
     postedOn: { type: Date, default: Date.now() },
     postedBy: { type: String }
