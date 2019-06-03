936072ae17156392db077bd37a7b58fd94b07eee
Vibhav-deo <vdeo@deakin.edu.au>
2019-06-03 17:53:37 +1000

    new routes imported

--- Routes/index.js
+++ Routes/index.js
@@ -7,6 +7,6 @@ var DemoBaseRoute = require('./demoRoute/demoBaseRoute');
 const StudentRoute = require('./studentRoute/studentRoute')
 const AgentRoute = require('./agentRoute/agentRoute')
 const CourseRoute = require('./courseRoute/courseRoute')
-
-var APIs = [].concat(DemoBaseRoute, StudentRoute, AgentRoute, CourseRoute);
+const NewsRoute = require('./newsRoute/newsRoute')
+var APIs = [].concat(DemoBaseRoute, StudentRoute, AgentRoute, CourseRoute, NewsRoute);
 module.exports = APIs;
