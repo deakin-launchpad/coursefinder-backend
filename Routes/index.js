424b9e5de693527ea2ce1c5aeef68cce707b9ae5
Vibhav-deo <vdeo@deakin.edu.au>
2019-06-06 16:27:21 +1000

    new routes import

--- Routes/index.js
+++ Routes/index.js
@@ -4,9 +4,10 @@
 'use strict'
 
 var DemoBaseRoute = require('./demoRoute/demoBaseRoute');
-const StudentRoute = require('./studentRoute/studentRoute')
-const AgentRoute = require('./agentRoute/agentRoute')
-const CourseRoute = require('./courseRoute/courseRoute')
-const NewsRoute = require('./newsRoute/newsRoute')
-var APIs = [].concat(DemoBaseRoute, StudentRoute, AgentRoute, CourseRoute, NewsRoute);
+const StudentRoute = require('./studentRoute/studentRoute');
+const AgentRoute = require('./agentRoute/agentRoute');
+const CourseRoute = require('./courseRoute/courseRoute');
+const NewsRoute = require('./newsRoute/newsRoute');
+const ApplicationRoute = require('./applicationRoute/applicationRoute')
+var APIs = [].concat(DemoBaseRoute, StudentRoute, AgentRoute, CourseRoute, NewsRoute, ApplicationRoute);
 module.exports = APIs;
